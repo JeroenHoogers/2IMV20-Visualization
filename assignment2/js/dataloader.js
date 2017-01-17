fc73e31c591403bd440d1390f567e97a36b1751a
JeroenHoogers <hoogers.jeroen@gmail.com>
2017-01-17 23:42:57 +0100

    Added some data to the map for testing

---
+++ assignment2/js/dataloader.js
@@ -0,0 +1,23 @@
+var countryMap = new Map();
+var landDist = new Map();
+
+function loaddata()
+{
+	// Load country metadata such that we can identify a country using the country code
+	d3.csv("data/country-codes.csv", function(error, data) {
+	    data.forEach(function(d) {
+	        countryMap.set(parseInt(d['M49']), 
+	        	{"name": d['name'],  "code": d['ISO3166-1-Alpha-3']}
+	        );
+	    });
+	});
+
+	d3.csv("data/landdist/API_AG.LND.TOTL.K2_DS2_en_csv_v2.csv", function(error, data) {
+	    data.forEach(function(d) {
+	        landDist.set(d['Country Code'], 
+	        	{"m_2015": d['2015']}
+	        );
+	    });
+	});
+
+}
